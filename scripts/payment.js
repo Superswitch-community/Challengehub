4c1df360a8a6d936ab7d36aa745ec846a581d296
Superswitch-community <greatajoku7@gmail.com>
2025-10-27 19:47:02 +0100

    Update payment.js

--- scripts/payment.js
+++ scripts/payment.js
@@ -83,7 +83,7 @@ async function payWithPaystack() {
             if (data && data.status === true && data.data && data.data.authorization_url) {
                 const authorizationUrl = data.data.authorization_url;
                 const reference = data.data.reference;
-                console.log(reference);
+               // console.log(reference);
                 localStorage.setItem('reference', reference);
                 //redirect the user to the authorization URL
                 window.location.href = authorizationUrl;
@@ -166,6 +166,7 @@ const updateUserPaymentStatus = async (category, userId, paymentStatus) => {
 document.getElementById('payment-button').addEventListener('click', () => {
     payWithPaystack();
 })
+
 
 
 
